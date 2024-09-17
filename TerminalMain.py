befc2ded2edb5c0e94ccd4e1d09e60e0f40807f3
Almad0 <sbrwillo@gmail.com>
2024-09-17 11:50:54 +0200

    First attempt textbox

--- TerminalMain.py
+++ TerminalMain.py
@@ -1,1 +1,19 @@
 import curses
+from curses import wrapper
+from curses.textpad import Textbox, rectangle
+import time
+
+def main(stdscr):
+
+    win = curses.newwin(3, 18, 2, 2, )
+    box = Textbox(win)
+    rectangle(stdscr, 2, 2, 5, 20)
+
+    stdscr.refresh()
+
+    box.edit()
+
+    stdscr.getch()
+
+
+wrapper (main)
